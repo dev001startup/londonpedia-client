eabf889396298d5756db84294e65c0adaa536737
David Seow <david.seow@gmail.com>
2013-12-23 08:07:11 +0800

    Populate detail page. WIP.

--- app/controllers/detailPage.js
+++ app/controllers/detailPage.js
@@ -1,9 +1,19 @@
 console.log("detailPage");
 
-// $.page.open();
+var args = arguments[0] || {}, currentEvent;
+
+currentEvent = _.where(Alloy.Globals.Events, {id: args.annotation.id})[0];
+
+$.header.text = currentEvent.title;
+$.photo.image = currentEvent.photo.url;
+$.startDate.text = currentEvent.start_date;
+$.endDate.text = currentEvent.end_date;
+$.address.text = currentEvent.venue.address;
 
 $.page.addEventListener('singletap', function() {
    Alloy.Globals.NAV.closeWin();
 });
 
-console.log(Alloy.Globals.Events);
+function doClick() {
+    // do something
+}
